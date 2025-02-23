c0a68cd97ee180f80fe1f8e0609b19bad087d7ee
AngelTenewaa <mataidoo2011@gmail.com>
2025-02-23 16:56:25 +0000

    Initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+StudentGrade = {
+    "Malaika" : {
+        "mathematics" : 15 ,
+        "Creative Arts" : 29 ,
+        "French" : 20 ,
+        "Social Studies"  : 21 ,
+        "English" : 21,
+        "Science" : 9/10
+    },
+    "Emmanuella" : {
+        "mathematics" : 10 ,
+        "Creative Arts" : 29 ,
+        "French" : 17 ,
+        "Social Studies"  : 26,
+        "English" : 29 ,
+    },
+}
+print(StudentGrade)
